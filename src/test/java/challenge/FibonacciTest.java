b4917175fde42b3b55947f6be5e46c834a845844
sroliveira <olivetree.silva@gmail.com>
2018-11-11 04:02:50 -0200

    Desafio Completo fibonacci e numbers em java

--- src/test/java/challenge/FibonacciTest.java
+++ src/test/java/challenge/FibonacciTest.java
@@ -9,43 +9,43 @@ import static junit.framework.TestCase.assertTrue;
 public class FibonacciTest {
 
     @Test
-    public void test1() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo0() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(0);
         assertEquals(result, 0);
     }
 
     @Test
-    public void test2() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo2() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(2);
         assertEquals(result, 1);
     }
 
     @Test
-    public void test3() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo8() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(8);
         assertEquals(result, 21);
     }
 
     @Test
-    public void test4() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo12() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(12);
         assertEquals(result, 144);
     }
 
     @Test
-    public void test5() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo18() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(18);
         assertEquals(result, 2584);
     }
 
     @Test
-    public void test6() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceTo22() throws InvalidFibonacciInputException {
         long result = Fibonacci.calculate(22);
         assertEquals(result, 17711);
     }
 
     @Test(expected = InvalidFibonacciInputException.class)
-    public void test7() throws InvalidFibonacciInputException {
+    public void shouldReturnFibonacciSequenceToNegativeNumber() throws InvalidFibonacciInputException {
         Fibonacci.calculate(-1);
     }
 
--- src/test/java/challenge/NumbersTest.java
+++ src/test/java/challenge/NumbersTest.java
@@ -14,81 +14,81 @@ public class NumbersTest {
     private int[] descendingSortedNumbers = {111, 74, 73, 49, 39, 32, 23, 19, 12, 11, 11, 2, -1};
 
     @Test(expected = InvalidPositionException.class)
-    public void test1() throws InvalidPositionException {
+    public void shouldThrowExceptionDueInvalidPositionInShortest() throws InvalidPositionException {
         Numbers.findNShortest(numbers, 340);
     }
 
     @Test(expected = InvalidPositionException.class)
-    public void test2() throws InvalidPositionException {
+    public void shouldThrowExceptionDueInvalidPositionInLargest() throws InvalidPositionException {
         Numbers.findNLargest(numbers, 340);
     }
 
     @Test(expected = InvalidPositionException.class)
-    public void test3() throws InvalidPositionException {
+    public void shouldThrowExceptionDueNegativePositionInLargest() throws InvalidPositionException {
         Numbers.findNLargest(numbers, -1);
     }
 
     @Test(expected = InvalidPositionException.class)
-    public void test4() throws InvalidPositionException {
+    public void shouldThrowExceptionDueNegativePositionInShortest() throws InvalidPositionException {
         Numbers.findNShortest(numbers, -1);
     }
 
     @Test
-    public void test5() throws InvalidPositionException {
+    public void shouldReturnTheLargestNumber() throws InvalidPositionException {
         int result = Numbers.findNLargest(numbers, 1);
         assertEquals(result, 111);
     }
 
     @Test
-    public void test6() throws InvalidPositionException{
+    public void shouldReturnTheSecondShortestNumber() throws InvalidPositionException{
         int result = Numbers.findNShortest(numbers, 2);
         assertEquals(result, 2);
     }
 
     @Test
-    public void test7() throws InvalidPositionException{
+    public void shouldReturnTheThirdShortestNumber() throws InvalidPositionException{
         int result = Numbers.findNShortest(numbers, 3);
         assertEquals(result, 11);
     }
 
     @Test
-    public void test8() throws InvalidPositionException  {
+    public void shouldReturnTheThirdLargestNumber() throws InvalidPositionException  {
         int result = Numbers.findNLargest(numbers, 3);
         assertEquals(result, 73);
     }
 
     @Test
-    public void test9() throws InvalidPositionException {
+    public void shouldReturnTheSeventhShortestNumber() throws InvalidPositionException {
         int result = Numbers.findNShortest(numbers, 7);
         assertEquals(result, 23);
     }
 
     @Test
-    public void test10() throws InvalidPositionException  {
+    public void shouldReturnTheFifthLargestNumber() throws InvalidPositionException  {
         int result = Numbers.findNLargest(numbers, 5);
         assertEquals(result, 39);
     }
 
     @Test
-    public void test11() throws InvalidPositionException {
+    public void shouldReturnTheShortestNumber() throws InvalidPositionException {
         int result = Numbers.findNShortest(numbers, 1);
         assertEquals(result, -1);
     }
 
     @Test
-    public void test12() {
+    public void shouldFindNumberInArray() {
         boolean result = Numbers.contains(numbers, 11);
         assertTrue(result);
     }
 
     @Test
-    public void test13() {
+    public void shouldNotFindNumberInArray() {
         boolean result = Numbers.contains(numbers, 390);
         assertFalse(result);
     }
 
     @Test
-    public void test14() {
+    public void shouldAscendingSortList() {
         int[] result = Numbers.ascendingSort(numbers);
 
         for (int i = 0; i < numbers.length; i++) {
@@ -97,7 +97,7 @@ public class NumbersTest {
     }
 
     @Test
-    public void test15() {
+    public void shouldDescendingSortList() {
         int[] result = Numbers.descendingSort(numbers);
 
         for (int i = 0; i < numbers.length; i++) {
